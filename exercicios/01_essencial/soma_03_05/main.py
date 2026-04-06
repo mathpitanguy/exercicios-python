3cea8a4acaad62c13e1c2070b18635ad3f399d98
mathpitanguy <estudospitanguy123@gmail.com>
2026-04-06 17:58:12 -0300

    Resolução do Exercício Soma 03 05

--- exercicios/01_essencial/soma_03_05/main.py
+++ exercicios/01_essencial/soma_03_05/main.py
@@ -1,3 +1,11 @@
 def resposta(n):
-    pass
+    n = int(n)
 
+    contador = 0
+    lista = list(range(n))
+
+    for item in lista:
+         if item % 3 == 0 or item % 5 == 0:
+            contador = contador + item
+
+    return contador
