7828e2046ebb8dca6b4da3d0ffa849b33433d776
mathpitanguy <estudospitanguy123@gmail.com>
2026-04-07 14:42:43 -0300

    Resolução do exercício Data Segundos

--- exercicios/01_essencial/data_segundos/main.py
+++ exercicios/01_essencial/data_segundos/main.py
@@ -1,2 +1,9 @@
 def resposta(dias, horas, minutos, segundos):
-    pass
+    dias = int(dias)
+    horas = int(horas)
+    minutos = int(minutos)
+    segundos = int (segundos)
+
+    resultado = ((dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos *60) + segundos)
+
+    return (resultado)
