3e6b0160ba159d428f804c7dd04550265309edbf
mathpitanguy <estudospitanguy123@gmail.com>
2026-04-07 14:32:42 -0300

    Resolução do exercício Apaga

--- exercicios/01_essencial/apaga/main.py
+++ exercicios/01_essencial/apaga/main.py
@@ -1,2 +1,9 @@
-def resposta(palavra, index):
-    pass
+def resposta (palavra, index):
+    palavra = str(palavra)
+    index = int(index)
+
+    if index > len(palavra):
+        return palavra
+    else:
+        novapalavra = palavra[:index] + palavra[index+1:]
+        return novapalavra
