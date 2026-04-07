6258aa8233a9a3bbd76b9a0664dd264b3b77cd7c
mathpitanguy <estudospitanguy123@gmail.com>
2026-04-07 15:36:04 -0300

    Resolução do exercício troca_letras

--- exercicios/01_essencial/troca_letras/main.py
+++ exercicios/01_essencial/troca_letras/main.py
@@ -1,2 +1,12 @@
-def resposta(s):
-    pass
+def resposta (s):
+    s = str(s)
+
+    if len(s) <= 1:
+        return s
+    else:
+        primeiro_carac = s[0]
+        ultimo_carac = s[-1]
+
+        palavra = ultimo_carac + s[1:-1] + primeiro_carac
+
+        return palavra
