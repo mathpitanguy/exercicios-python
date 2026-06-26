528c239429e19d01a477707847ac83a0961d9cb5
Matheus Pitanguy <estudospitanguy123@gmail.com>
2026-06-26 18:52:06 +0000

    resolve os exercícios fibonacci, divisor_comum, crescimento_populacao e usuario_senha

--- exercicios/01_essencial/fibonacci/main.py
+++ exercicios/01_essencial/fibonacci/main.py
@@ -1,2 +1,6 @@
 def resposta(n):
-    pass
+    fibonacci = [1,1]
+    while len(fibonacci) < n:
+        resultado = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(resultado)
+    return (fibonacci[-1])
